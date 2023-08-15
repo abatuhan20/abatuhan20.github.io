4ed5d13de4c77c6ea1f7dcdff5e9c1e1b48752d3
Arda AYDIN <92793670+abatuhan20@users.noreply.github.com>
2023-08-15 13:40:42 +0300

    balls reduced viewport fixed

--- src/constants/index.js
+++ src/constants/index.js
@@ -54,22 +54,6 @@ import {
   
   const technologies = [
     {
-      name: "HTML 5",
-      icon: html,
-    },
-    {
-      name: "CSS 3",
-      icon: css,
-    },
-    {
-      name: "JavaScript",
-      icon: javascript,
-    },
-    {
-      name: "TypeScript",
-      icon: typescript,
-    },
-    {
       name: "React JS",
       icon: reactjs,
     },
@@ -89,10 +73,6 @@ import {
     {
       name: "Three JS",
       icon: threejs,
-    },
-    {
-      name: "git",
-      icon: git,
     },
     
   ];
